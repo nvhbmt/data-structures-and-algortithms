ac870b84be81e4c78c783380b726523ced7eb409
Nguyen Hoang <cunpoi1357@gmail.com>
2024-10-09 23:52:00 +0700

    Subsets

---
+++ src/LeetCode/Subsets.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+    private void backtracking(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
+        list.add(new ArrayList<>(tempList));
+        for (int i = start; i < nums.length; i++) {
+            tempList.add(nums[i]);
+            backtracking(list, tempList, nums, i + 1);
+            tempList.removeLast();
+        }
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        backtracking(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+}
