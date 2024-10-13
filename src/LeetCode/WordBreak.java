9c2bd2e22ba96b93e9aaeec9578b0f3abe653941
Nguyen Hoang <cunpoi1357@gmail.com>
2024-10-13 22:08:22 +0700

    WordBreak

---
+++ src/LeetCode/WordBreak.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+import java.util.List;
+
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (String word : wordDict) {
+                int start = i - word.length();
+                if (start >= 0 && dp[start] && s.substring(start, i).equals(word)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length() + 1];
+    }
+}
