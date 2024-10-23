72f307dd7b7f1a0c33ba013596af9a9aa1a21be3
Nguyen Hoang <cunpoi1357@gmail.com>
2024-10-23 23:19:39 +0700

    Final Prices With a Special Discount in a Shop

---
+++ src/LeetCode/FinalPricesWithASpecialDiscountInAShop.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class FinalPricesWithASpecialDiscountInAShop {
+    public int[] finalPrices(int[] prices) {
+        int[] result = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            result[i] = prices[i];
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] <= prices[i]) {
+                    result[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
