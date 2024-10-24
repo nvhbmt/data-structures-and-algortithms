6c8f9335f9bd121e01651c164ec86484da4ca3e8
Nguyen Hoang <cunpoi1357@gmail.com>
2024-10-24 19:32:45 +0700

    Trapping Rain Water

---
+++ src/LeetCode/TrappingRainWater.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class TrappingRainWater {
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = height[left], rightMax = height[right];
+        int water = 0;
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = Math.max(leftMax, height[left]);
+                water += leftMax - height[left];
+            } else {
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+                water += rightMax - height[right];
+            }
+        }
+        return water;
+    }
+}
