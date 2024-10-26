86ffb6eafefec139e92fbc263f5ebed876c378aa
Nguyen Hoang <cunpoi1357@gmail.com>
2024-10-26 10:35:13 +0700

    Sqrt(x)

---
+++ src/LeetCode/SqrtX.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class SqrtX {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        int start = 1, end = x;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid == x / mid) return mid;
+            if (mid < x / mid) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return end;
+    }
+}
