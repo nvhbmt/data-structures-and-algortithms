562558f459b15ae6d1cee35925623c073d9f72fa
Nguyen Hoang <cunpoi1357@gmail.com>
2024-11-05 21:47:24 +0700

    Count Complete Tree Nodes

---
+++ src/LeetCode/CountCompleteTreeNodes.java
@@ -0,0 +1,10 @@
+package LeetCode;
+
+import Tree.TreeNode;
+
+public class CountCompleteTreeNodes {
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
