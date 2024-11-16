636b02fa3600d78b1217820b059d0cfe7d1cef81
Nguyen Hoang <cunpoi1357@gmail.com>
2024-11-16 21:02:51 +0700

    Pascal's Triangle

---
+++ src/LeetCode/PascalsTriangle.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>(numRows);
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>(i + 1);
+            row.add(1);
+            if (i > 0) {
+                for (int j = 1; j < i; j++) {
+                    row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+                row.add(1);
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
